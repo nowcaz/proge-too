904fc7563be0b7f6c3f69a889cf7bcbe6d708c20
Opilane TTHK <opilane@tthk.ee>
2024-11-28 11:58:21 +0200

    Lisasin reCAPTCHA valiku teha, et kas oled robot või mitte

--- M-ng-main/script.js
+++ M-ng-main/script.js
@@ -5,21 +5,17 @@ let iconCart = document.querySelector('.icon-cart');
 let body = document.querySelector('body');
 let closeCart = document.querySelector('.close');
 
+iconCart.addEventListener('click', toggleCart);
+closeCart.addEventListener('click', toggleCart);
 
-iconCart.addEventListener('click', () => {
+function toggleCart() {
     body.classList.toggle('showCart');
-});
-
-closeCart.addEventListener('click', () => {
-    body.classList.toggle('showCart');
-});
+}
 
 function addToCart(productName, price, sizeId) {
     const selectedSize = document.getElementById(sizeId).value;
-    
 
     let positionThisProductInCart = cart.findIndex(item => item.name === productName && item.size === selectedSize);
-    
 
     if (positionThisProductInCart < 0) {
         cart.push({ name: productName, price: price, size: selectedSize, quantity: 1 });
@@ -36,12 +32,12 @@ function addToCart(productName, price, sizeId) {
 function displayCartItems() {
     listCartHTML.innerHTML = '<h3>Ostukorv</h3>';
     let total = 0;
-    
+
     cart.forEach(item => {
         listCartHTML.innerHTML += `<p>${item.name} (Suurus: ${item.size}) - ${item.price}€ x ${item.quantity}</p>`;
         total += item.price * item.quantity;
     });
-    
+
     listCartHTML.innerHTML += `<p><strong>Kokku: ${total}€</strong></p>`;
 }
 
